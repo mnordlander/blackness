03fd21cf32e367ab7bf7054c99f8b8a4f9e70a48
Mathias Nordlander <mnordlander@ct-group.com>
2022-02-02 00:00:52 +0100

    Small changes in app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 function toggleLogin() {
     $('#login-user').toggleClass('login-user-hide');
     $('#login-user').toggleClass('login-user-show');
-    $('body').toggleClass('login-body-hide');
-    $('body').toggleClass('login-body-show');
 
 };
