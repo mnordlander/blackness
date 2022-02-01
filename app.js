92cfddd44f3746f5d49d091a52dbdd72894d94a9
Mathias Nordlander <mnordlander@ct-group.com>
2022-02-01 23:58:08 +0100

    Animation for login

---
+++ app.js
@@ -0,0 +1,7 @@
+function toggleLogin() {
+    $('#login-user').toggleClass('login-user-hide');
+    $('#login-user').toggleClass('login-user-show');
+    $('body').toggleClass('login-body-hide');
+    $('body').toggleClass('login-body-show');
+
+};
